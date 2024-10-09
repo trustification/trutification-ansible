97d941f23345052cc8cd52ac5b4093dbc120ee73
Gilles Dubreuil <gdubreui@redhat.com>
2024-10-09 14:19:21 +0200

    Add SAN to CSR

--- bin/gencerts.sh
+++ bin/gencerts.sh
@@ -2,8 +2,7 @@
 
 SUBJ='/C=US/ST=North Carolina/L=Raleigh'
 SUBJ+='/O=Red Hat Inc./OU=RHTPA'
-CN='CN=192.168.121.60' 
-
+CN='/CN=localhost' 
 TRUST_ANCHOR='rootCA'
 
 CERTS=(
@@ -19,12 +18,31 @@ CERTS=(
     'vexination-api-tls'
 )
 
+
+cat > /tmp/rhtpa/certs/openssl.cnf <<EOF
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+prompt = no
+
+[req_distinguished_name]
+CN = 192.168.121.60
+
+[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+
+[alt_names]
+IP.1 = 192.168.121.60
+EOF
+
 CERT_DIR='/tmp/rhtpa/certs'
 mkdir -p "$CERT_DIR"
 
 # Trust Anchor
 openssl genpkey -algorithm RSA -out "${CERT_DIR}/${TRUST_ANCHOR}.key" -outform PEM -pkeyopt rsa_keygen_bits:2048
-openssl req -x509 -new -nodes -key "${CERT_DIR}/${TRUST_ANCHOR}.key" -sha256 -days 3650 -out "${CERT_DIR}/${TRUST_ANCHOR}.crt" -subj "${SUBJ}${CN}"
+openssl req -x509 -new -nodes -key "${CERT_DIR}/${TRUST_ANCHOR}.key" -sha256 -days 3650 -out "${CERT_DIR}/${TRUST_ANCHOR}.crt" -subj "${SUBJ}/CN=RTHTPA Root CA"
 
 for certname in "${CERTS[@]}"; do
     rm -f "${CERT_DIR}/${certname}.key" "${CERT_DIR}/${certname}.crt"
@@ -33,7 +51,7 @@ for certname in "${CERTS[@]}"; do
     openssl genpkey -algorithm RSA -out "${CERT_DIR}/${certname}.key" -outform PEM -pkeyopt rsa_keygen_bits:2048
 
     # # Create the CSR
-    openssl req -new -key "${CERT_DIR}/${certname}.key" -out "${CERT_DIR}/${certname}.csr" -subj "${SUBJ}${CN}"
+    openssl req -new -key "${CERT_DIR}/${certname}.key" -out "${CERT_DIR}/${certname}.csr" -subj "${SUBJ}/CN=${certname}" -config /tmp/rhtpa/certs/openssl.cnf
 
     # # Sign the CSR with the CA
     openssl x509 -req -in "${CERT_DIR}/${certname}.csr" -CA "${CERT_DIR}/${TRUST_ANCHOR}.crt" -CAkey "${CERT_DIR}/${TRUST_ANCHOR}.key" -CAcreateserial -out "${CERT_DIR}/${certname}.crt" -days 825 -sha256
