dfe87a6fc1e173221e1d420a99c05650595b3b4b
Gilles Dubreuil <gdubreui@redhat.com>
2024-10-09 12:27:28 +0200

    Add IP to Common Name

--- bin/gencerts.sh
+++ bin/gencerts.sh
@@ -2,6 +2,7 @@
 
 SUBJ='/C=US/ST=North Carolina/L=Raleigh'
 SUBJ+='/O=Red Hat Inc./OU=RHTPA'
+CN='CN=192.168.121.60' 
 
 TRUST_ANCHOR='rootCA'
 
@@ -23,7 +24,7 @@ mkdir -p "$CERT_DIR"
 
 # Trust Anchor
 openssl genpkey -algorithm RSA -out "${CERT_DIR}/${TRUST_ANCHOR}.key" -outform PEM -pkeyopt rsa_keygen_bits:2048
-openssl req -x509 -new -nodes -key "${CERT_DIR}/${TRUST_ANCHOR}.key" -sha256 -days 3650 -out "${CERT_DIR}/${TRUST_ANCHOR}.crt" -subj "${SUBJ}/CN=RTHTPA Example Root CA"
+openssl req -x509 -new -nodes -key "${CERT_DIR}/${TRUST_ANCHOR}.key" -sha256 -days 3650 -out "${CERT_DIR}/${TRUST_ANCHOR}.crt" -subj "${SUBJ}${CN}"
 
 for certname in "${CERTS[@]}"; do
     rm -f "${CERT_DIR}/${certname}.key" "${CERT_DIR}/${certname}.crt"
@@ -32,7 +33,7 @@ for certname in "${CERTS[@]}"; do
     openssl genpkey -algorithm RSA -out "${CERT_DIR}/${certname}.key" -outform PEM -pkeyopt rsa_keygen_bits:2048
 
     # # Create the CSR
-    openssl req -new -key "${CERT_DIR}/${certname}.key" -out "${CERT_DIR}/${certname}.csr" -subj "${SUBJ}/CN=${certname}"
+    openssl req -new -key "${CERT_DIR}/${certname}.key" -out "${CERT_DIR}/${certname}.csr" -subj "${SUBJ}${CN}"
 
     # # Sign the CSR with the CA
     openssl x509 -req -in "${CERT_DIR}/${certname}.csr" -CA "${CERT_DIR}/${TRUST_ANCHOR}.crt" -CAkey "${CERT_DIR}/${TRUST_ANCHOR}.key" -CAcreateserial -out "${CERT_DIR}/${certname}.crt" -days 825 -sha256
