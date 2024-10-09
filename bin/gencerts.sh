c439d11fa145effaafb86f9a1bd4d775075a655a
Gilles Dubreuil <gdubreui@redhat.com>
2024-10-09 09:07:48 +0200

    /tmp for gencerts; custom root anchor with json

--- bin/gencerts.sh
+++ bin/gencerts.sh
@@ -3,6 +3,8 @@
 SUBJ='/C=US/ST=North Carolina/L=Raleigh'
 SUBJ+='/O=Red Hat Inc./OU=RHTPA'
 
+TRUST_ANCHOR='rootCA'
+
 CERTS=(
     'bombastic-api-tls'
     'collector-osv-tls'
@@ -16,28 +18,28 @@ CERTS=(
     'vexination-api-tls'
 )
 
-mkdir -p certs
-cd certs
+CERT_DIR='/tmp/rhtpa/certs'
+mkdir -p "$CERT_DIR"
 
 # Trust Anchor
-openssl genpkey -algorithm RSA -out rootCA.key -aes256 -pass pass:passwd123 -outform PEM -pkeyopt rsa_keygen_bits:2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.crt -passin pass:passwd123 -subj "${SUBJ}/CN=RTHTPA Example Root CA"
+openssl genpkey -algorithm RSA -out "${CERT_DIR}/${TRUST_ANCHOR}.key" -aes256 -pass pass:passwd123 -outform PEM -pkeyopt rsa_keygen_bits:2048
+openssl req -x509 -new -nodes -key "${CERT_DIR}/${TRUST_ANCHOR}.key" -sha256 -days 3650 -out "${CERT_DIR}/${TRUST_ANCHOR}.crt" -passin pass:passwd123 -subj "${SUBJ}/CN=RTHTPA Example Root CA"
 
 for certname in "${CERTS[@]}"; do
-    rm -f "${certname}.key" "${certname}.crt"
+    rm -f "${CERT_DIR}/${certname}.key" "${CERT_DIR}/${certname}.crt"
     
     # Create the private key
-    openssl genpkey -algorithm RSA -out "${certname}.key" -aes256 -pass pass:passwd123 -outform PEM -pkeyopt rsa_keygen_bits:2048
+    openssl genpkey -algorithm RSA -out "${CERT_DIR}/${certname}.key" -aes256 -pass pass:passwd123 -outform PEM -pkeyopt rsa_keygen_bits:2048
 
     # # Create the CSR
-    openssl req -new -key "${certname}.key" -out ${certname}.csr -passin pass:passwd123 -subj "${SUBJ}/CN=${certname}"
+    openssl req -new -key "${CERT_DIR}/${certname}.key" -out "${CERT_DIR}/${certname}.csr" -passin pass:passwd123 -subj "${SUBJ}/CN=${certname}"
 
     # # Sign the CSR with the CA
-    openssl x509 -req -in "${certname}.csr" -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out "${certname}.crt" -days 825 -sha256 -passin pass:passwd123
+    openssl x509 -req -in "${CERT_DIR}/${certname}.csr" -CA "${CERT_DIR}/${TRUST_ANCHOR}.crt" -CAkey "${CERT_DIR}/${TRUST_ANCHOR}.key" -CAcreateserial -out "${CERT_DIR}/${certname}.crt" -days 825 -sha256 -passin pass:passwd123
 
-    # Remove the CSR
-    rm "${certname}.csr"
+    # Remove the CSR file
+    rm "${CERT_DIR}/${certname}.csr"
 done
 
 # Remove the CA serial file
-rm rootCA.srl
+rm "${CERT_DIR}/${TRUST_ANCHOR}.srl"
